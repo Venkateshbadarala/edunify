1dcacf11ada4d67fbea545d7c4b9dbefdda72a3f
Venkatesh Badarala <21PA1A1206@vishnu.edu.in>
2024-07-26 16:02:42 +0530

    modified..

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ const nextConfig = {
     images: {
       domains: ['res.cloudinary.com', 'placehold.co'],
     },
+    env: {
+      MYSQL_HOST: process.env.MYSQL_HOST,
+      MYSQL_USER: process.env.MYSQL_USER,
+      MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
+      MYSQL_DATABASE: process.env.MYSQL_DATABASE,
+    },
   };
   
   export default nextConfig;
